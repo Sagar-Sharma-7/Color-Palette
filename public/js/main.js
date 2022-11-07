74cebcc26ecff44d9f9003b943a365991d93507e
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2022-11-07 20:42:48 +0530

    background color with js

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,22 @@
-const palette = document.querySelectorAll(".palette")
+const palette = document.querySelectorAll(".palette");
+const circle = document.querySelectorAll(".circle");
+const hexcode = document.querySelectorAll(".hexcode");
+
+
+const colorList = [
+    '#9100B5',
+    '#B000DC',
+    '#CC00FF',
+    '#D52BFF',
+    '#E167FF',
+    '#E789FF',
+    '#EEABFF',
+    '#F5CDFF',
+]
+
+let n = circle.length;
+let i;
+for(i = 0; i < n; i++){
+    hexcode[i].innerHTML = colorList[i]
+    circle[i].style.backgroundColor = colorList[i]
+}
