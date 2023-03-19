f022a55f4d531f8f5f4de11be67fb03fcff5a474
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2023-03-20 03:14:08 +0530

    copying hexcode of palette

--- public/js/main.js
+++ public/js/main.js
@@ -29,62 +29,13 @@ const hexcode = document.querySelectorAll(".hexcode");
 
 
 const colorList = [
-    '#9100B5',
-    '#B000DC',
-    '#CC00FF',
-    '#D52BFF',
-    '#E167FF',
-    '#E789FF',
-    '#EEABFF',
-    '#F5CDFF',
-    '#C04100',
-    '#CE4500',
-    '#E04B00',
-    '#EB5103',
-    '#FF5600',
-    '#FF6618',
-    '#FF722B',
-    '#FF7B38',
-    '#0005A3',
-    '#0007C7',
-    '#0008E6',
-    '#0008FA',
-    '#050DFF',
-    '#1A21FF',
-    '#383FFF',
-    '#4D52FF',
-    '#1AB800',
-    '#1BBE00',
-    '#1DCE00',
-    '#1FDD00',
-    '#21EE00',
-    '#24FF00',
-    '#46FF28',
-    '#9CFF8C',
-    '#00AF9E',
-    '#00DEC8',
-    '#00F5DD',
-    '#06CFBB',
-    '#10FFE7',
-    '#32FFEA',
-    '#57FFEE',
-    '#88FFF3',
-    '#AD0000',
-    '#BC0000',
-    '#C90000',
-    '#DE0000',
-    '#EF0707',
-    '#FD0707',
-    '#FF1B1B',
-    '#FF4747',
-    '#D11D3C',
-    '#E72244',
-    '#F92F51',
-    '#FF385B',
-    '#FF4868',
-    '#FF5472',
-    '#FD627D',
-    '#FB738B',
+    '#9100B5','#B000DC','#CC00FF','#D52BFF','#E167FF','#E789FF','#EEABFF','#F5CDFF', // Purple
+    '#C04100','#CE4500','#E04B00','#EB5103','#FF5600','#FF6618','#FF722B','#FF7B38', // Orange
+    '#0005A3','#0007C7','#0008E6','#0008FA','#050DFF','#1A21FF','#383FFF','#4D52FF', // Blue
+    '#1AB800','#1BBE00','#1DCE00','#1FDD00','#21EE00','#24FF00','#46FF28','#9CFF8C', // Green
+    '#00AF9E','#00DEC8','#00F5DD','#06CFBB','#10FFE7','#32FFEA','#57FFEE','#88FFF3', // Turquoise
+    '#AD0000','#BC0000','#C90000','#DE0000','#EF0707','#FD0707','#FF1B1B','#FF4747', // Red
+    '#D11D3C','#E72244','#F92F51','#FF385B','#FF4868','#FF5472','#FD627D','#FB738B', // Pink
 ];
 
 
@@ -93,11 +44,11 @@ let i;
 for(i = 0; i < n; i++){
     hexcode[i].innerHTML = colorList[i];
     circle[i].style.backgroundColor = colorList[i];
-    circle[i].title = i
+    circle[i].title = colorList[i]
 }
 
 circle.forEach(item => {
     item.addEventListener("click", () => {
-        console.log(colorList[item.title]);
+        navigator.clipboard.writeText(item.title);
     })
 });
