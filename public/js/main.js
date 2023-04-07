9395c82e99e4489f116fbba7eff69fadedd9974a
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2023-04-07 05:43:24 +0530

    turquoise palette correct arrangement

--- public/js/main.js
+++ public/js/main.js
@@ -33,7 +33,7 @@ const colorList = [
     '#C04100','#CE4500','#E04B00','#EB5103','#FF5600','#FF6618','#FF722B','#FF7B38', // Orange
     '#0005A3','#0007C7','#0008E6','#0008FA','#050DFF','#1A21FF','#383FFF','#4D52FF', // Blue
     '#1AB800','#1BBE00','#1DCE00','#1FDD00','#21EE00','#24FF00','#46FF28','#9CFF8C', // Green
-    '#00AF9E','#00DEC8','#00F5DD','#06CFBB','#10FFE7','#32FFEA','#57FFEE','#88FFF3', // Turquoise
+    '#00AF9E','#06CFBB','#00DEC8','#00F5DD','#10FFE7','#32FFEA','#57FFEE','#88FFF3', // Turquoise
     '#AD0000','#BC0000','#C90000','#DE0000','#EF0707','#FD0707','#FF1B1B','#FF4747', // Red
     '#D11D3C','#E72244','#F92F51','#FF385B','#FF4868','#FF5472','#FD627D','#FB738B', // Pink
 ];
